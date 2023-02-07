2bcfcb142535223ea821e047d5b8415bed9400ff
debuggersavljevich <sbogosavljevich@gmail.com>
2023-02-07 01:57:33 -0300

    add stuff

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StatusBar } from 'react-native'
+import CalculadoraScreen from './src/screens/CalculadoraScreen'
+import {styles} from './src/theme/appTheme'
 
 const App = () => {
   return (
-  
-    <View>
-      <Text>
-        Hola!
-      </Text>
-    </View>
+    
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar
+        backgroundColor={'black'}
+        barStyle={'light-content'}
+      />
 
+      <CalculadoraScreen/>
+
+    </SafeAreaView>
   )
 }
 
---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import {styles} from '../theme/appTheme'
+
+const CalculadoraScreen = () => {
+  return (
+  <View>
+    <Text style={styles.text}>
+        Hola
+    </Text>
+  </View>
+  )
+}
+
+export default CalculadoraScreen
+
